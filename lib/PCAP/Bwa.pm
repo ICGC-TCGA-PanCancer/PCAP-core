01f2a058b42c4fd501fd5565ccccd72c15cac1c5
Keiran Raine <keiranmraine@gmail.com>
2014-03-05 22:22:08 +0000

    minor edit to remove un-needed flag correction when working on bwa mem output

--- lib/PCAP/Bwa.pm
+++ lib/PCAP/Bwa.pm
@@ -38,7 +38,7 @@ const my $BWA_ALN => q{ aln%s -t %s -f %s_%s.sai %s %s.%s};
 const my $BAMFASTQ => q{ exclude=QCFAIL,SECONDARY,SUPPLEMENTARY T=%s S=%s O=%s O2=%s filename=%s};
 const my $BWA_MEM => q{ mem%s -T 0 -R %s -t %s %s};
 const my $ALN_TO_SORTED => q{ sampe -P -a 1000 -r '%s' %s %s_1.sai %s_2.sai %s.%s %s.%s | %s fixmate=1 inputformat=sam level=1 tmpfile=%s_tmp O=%s_sorted.bam};
-const my $BAMSORT => q{ fixmate=1 inputformat=sam level=1 tmpfile=%s_tmp O=%s_sorted.bam inputthreads=%s outputthreads=%s};
+const my $BAMSORT => q{ inputformat=sam level=1 tmpfile=%s_tmp O=%s_sorted.bam inputthreads=%s outputthreads=%s};
 
 sub bwa_version {
   my $bwa = which('bwa');
