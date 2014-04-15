cdfbdc11bb3e00cf5087ba9924243b8a580848a0
Keiran Raine <keiranmraine@gmail.com>
2014-04-15 12:47:09 +0100

    Upgrade to libmaus/biobambam to resolve install issues on CentOS

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@
 SOURCE_BWA="https://github.com/lh3/bwa/archive/0.7.7.tar.gz"
 SOURCE_SNAPPY="https://snappy.googlecode.com/files/snappy-1.1.1.tar.gz"
 SOURCE_IOLIB="http://downloads.sourceforge.net/project/staden/io_lib/1.13.4/io_lib-1.13.4.tar.gz"
-# remove patch from LIBMAUS section when next release available
-SOURCE_LIBMAUS="https://github.com/gt1/libmaus/archive/0.0.108-release-20140319092837.tar.gz"
-SOURCE_BIOBAMBAM="https://github.com/gt1/biobambam/archive/0.0.129-release-20140319092922.tar.gz"
+SOURCE_LIBMAUS="https://github.com/gt1/libmaus/archive/0.0.112-release-20140411095503.tar.gz"
+SOURCE_BIOBAMBAM="https://github.com/gt1/biobambam/archive/0.0.131-release-20140411101450.tar.gz"
 SOURCE_SAMTOOLS="https://github.com/samtools/samtools/archive/0.1.19.tar.gz"
 
 done_message () {
@@ -161,7 +160,6 @@ if [[ ",$COMPILE," == *,biobambam,* ]] ; then
     (
       get_distro "libmaus" $SOURCE_LIBMAUS
       cd $SETUP_DIR/libmaus
-      patch "src/libmaus/aio/PosixFdInput.hpp" < "$INIT_DIR/patches/fileModeFix.diff"
       autoreconf -i -f
       ./configure --prefix=$INST_PATH --with-snappy=$INST_PATH --with-io_lib=$INST_PATH
       make -j$CPU
