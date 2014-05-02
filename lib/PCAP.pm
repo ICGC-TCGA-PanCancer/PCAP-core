b89bc06f558c73930f4c9866c72ad1c5041cd867
Keiran Raine <keiranmraine@users.noreply.github.com>
2014-05-02 17:37:21 +0100

    Correct issue in upgrade path for bwa
    Need to improve this in future releases

--- lib/PCAP.pm
+++ lib/PCAP.pm
@@ -35,11 +35,11 @@ const my $LICENSE =>
 const my $DEFAULT_PATH => 'biobambam,samtools,bwa';
 const my %UPGRADE_PATH => ( '0.1.0'  => 'biobambam,samtools,bwa',
                             '0.1.1'  => 'biobambam,bwa',
-                            '0.1.2'  => 'biobambam',
-                            '0.2.0'  => 'biobambam',
-                            '0.2.99' => 'biobambam',
-                            '0.3.0'  => 'biobambam',
-                            '1.0.0'  => 'biobambam',
+                            '0.1.2'  => 'biobambam,bwa',
+                            '0.2.0'  => 'biobambam,bwa',
+                            '0.2.99' => 'biobambam,bwa',
+                            '0.3.0'  => 'biobambam,bwa',
+                            '1.0.0'  => 'biobambam,bwa',
                             '1.0.1'  => 'biobambam,bwa',
                             '1.0.2'  => '',
                           );
