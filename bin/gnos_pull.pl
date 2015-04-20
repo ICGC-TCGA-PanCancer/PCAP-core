8ebbad37fc98033f17df52757be3ed91f105dd84
Keiran Raine <keiranmraine@gmail.com>
2015-04-20 14:57:10 +0100

    Tiny change that allows calls and alignments to be pulled to same out location in different processes, previously blocked

--- bin/gnos_pull.pl
+++ bin/gnos_pull.pl
@@ -56,7 +56,7 @@ const my $GTDL_COMMAND => '%s%s -v -c %s -d %scghub/data/analysis/download/%s -p
   my $options = option_builder();
 
   my $proc = Proc::PID::File->new(-verify => 1);
-  $proc->file('dir' => $options->{'outdir'});
+  $proc->file('dir' => $options->{'outdir'}, 'name' => 'gnos_pull.pl.'.$options->{'analysis'});
   if($proc->alive) {
     warn "Already running against output location: '$options->{outdir}' ... exiting!\n" if($options->{'debug'});
     exit 0;
